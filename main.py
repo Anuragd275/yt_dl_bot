3d965bcf0061bea88f7b70d0db0c993c2f331f53
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-02-06 18:56:16 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import telebot
-
-import time
-
-import os
-
 from app import *
 
 TOKEN = "5823709070:AAH3K8f36_ZTi-XoYzrWBb8QA9wIkb8CPbU"
@@ -11,41 +6,56 @@ TOKEN = "5823709070:AAH3K8f36_ZTi-XoYzrWBb8QA9wIkb8CPbU"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start", "help"])
-
 def welcome_message(message):
-
     chat_id = message.chat.id
+    bot.send_message(chat_id, "Hi, I can download YouTube videos for you :)")
+    bot.send_message(chat_id, "Send /video <video resolution>  <video link> to download video")
+    bot.send_message(chat_id, "Send /audio <video url> to download audio")
 
-    bot.send_message(chat_id, "Hi, I can do something for you :)")
+# Handeling audio requests
 
-    bot.send_message(chat_id, "send /video <video link> to proceed")
-
-@bot.message_handler(commands=["video"])
-
-def video_dwonloader(message):
-
+@bot.message_handler(commands=["audio"])
+def audio_dwonloader(message):
     chat_id = message.chat.id
-
     message_text = message.text
-
     splitted_message = message_text.split()
-
     link = splitted_message[-1]
-
     video_details = video_info(link)
-
-    bot.send_message(chat_id, f"Here's what Youtube gave me \n\n{video_details}")
-
     file_title = title_finder(link)
-
-    bot.send_message(chat_id, f"Sending...{file_title}")
-
     the_audio_file = audio_dl(link)
+    new = replace_characters(file_title)
+    audio_to_send = open(f'{new}.mp4', 'rb')
+    bot.send_audio(chat_id, audio_to_send, caption=video_details)
 
-    audio_to_send = open(f'{file_title}.mp4', 'rb')
+# Handeling video requests 
+@bot.message_handler(commands=["video"])
+def video_downloader(message):
+    chat_id = message.chat.id
+    message_text = message.text
+    splitted_message = message_text.split()
+    link = splitted_message[-1]
+    res = splitted_message[-2]
+    video_details = video_info(link)
+    bot.send_message(chat_id, f"Here's what Youtube gave me \n\n{video_details}")
+    file_title = title_finder(link)
+    new = replace_characters(file_title)
+    if res == "360":
+        bot.send_message(chat_id,"Downloading in 360p resolution")
+        result_video_file = video_dl_360(link)
+        result_video_file_to_send = open(f'{new}.mp4', 'rb')
+        bot.send_video(chat_id, result_video_file_to_send)
+    elif res == "720":
+        bot.send_message(chat_id,"Downloading in 720p resolution")
+        result_video_file = video_dl_720(link)
+        result_video_file_to_send = open(f'{new}.mp4', 'rb')
+        bot.send_video(chat_id, result_video_file_to_send)
+    elif res == "1080":
+        bot.send_message(chat_id,"Downloading in 1080p resolution")
+        result_video_file = video_dl_1080(link)
+        result_video_file_to_send = open(f'{new}.mp4', 'rb')
+        bot.send_video(chat_id, result_video_file_to_send)
+    else:
+        bot.send_message("Invalid resolution selection ;_;")
 
-    bot.send_audio(chat_id, audio_to_send)
-
-    audio_to_send.close()
 
 bot.infinity_polling()
