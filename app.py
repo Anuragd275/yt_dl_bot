41898b2437bffe67e5fc3ccac6ffc60fdd94cc12
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-02-06 18:58:21 +0530

    :hammer:
    added | & , extractor

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
 from pytube import YouTube
+import os
+
+#Getting video details
 
 def video_info(video_url):
     yt = YouTube(video_url)
-
     title = yt.title
-    channel = yt.author
     views = yt.views
-    date = yt.publish_date
+    return(f"Title: {title}\n\nViews: {views}")
 
-    return(f"Title: {title} \n\nChannel name: {channel} \n\nViews: {views} \n\nPublished on: {date}")
-
+# audio requests
 def audio_dl(video_url):
     audio_file = YouTube(video_url)
 
     audio_file.streams.filter(only_audio=True)
     audio_to_send = audio_file.streams.get_by_itag(140)
-    audio_to_send.download()
+    song = audio_to_send.download()
+    return(song)
 
+# video in 360p
+def video_dl_360(video_url):
+    video_file = YouTube(video_url)
+
+    video_file_to_send = video_file.streams.get_by_itag(18)
+    video_360 = video_file_to_send.download()
+    return(video_360)
+
+
+# video in 720p
+def video_dl_720(video_url):
+    video_file = YouTube(video_url)
+
+    video_file_to_send = video_file.streams.get_by_itag(22)
+    video_720 = video_file_to_send.download()
+    return(video_720)
+
+# video in 1080p
+def video_dl_1080(video_url):
+    video_file = YouTube(video_url)
+
+    video_file_to_send = video_file.streams.get_by_itag(137)
+    video_1080 = video_file_to_send.download()
+    return(video_1080)
 
 def title_finder(video_url):
     vid = YouTube(video_url)
@@ -24,7 +49,8 @@ def title_finder(video_url):
     title_of_video = vid.title
     return(title_of_video)
 
-
-
-
-#audio_dl("https://youtu.be/dU8ZSWck7KQ")
+# replace "|" and "," from the title without this function everything crashes ;_;
+def replace_characters(string):
+    string = string.replace("|", "")
+    string = string.replace(",", "")
+    return string
